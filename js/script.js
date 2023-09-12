bf043ebf8237a2d519ca02cfb7b08a408299bbf7
tolulope-christ <lopebenzene@gmail.com>
2023-09-12 22:01:56 +0100

    add update

--- js/script.js
+++ js/script.js
@@ -21,20 +21,60 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
 	if(mode == 'light'){
-		document.getElementById('theme-style').href = static + '/blue.css'
+		document.getElementById('theme-style').href = '/frontend/css/blue.css'
 	}
 
 	if(mode == 'blue'){
-		document.getElementById('theme-style').href = static + '/default.css'
+		document.getElementById('theme-style').href = '/frontend/css/default.css'
 	}
 
 	if(mode == 'green'){
-		document.getElementById('theme-style').href = static + '/green.css'
+		document.getElementById('theme-style').href = '/frontend/css/green.css'
 	}
 
 	if(mode == 'purple'){
-		document.getElementById('theme-style').href = static + '/purple.css'
+		document.getElementById('theme-style').href = '/frontend/css/purple.css'
 	}
 
 	localStorage.setItem('theme', mode)
+};
+
+
+
+let menu = 'closed'
+
+let menuIcon = document.getElementById('menu-icon')
+let navigation = document.getElementById('navigation')
+let closeBtn = document.getElementById('close-btn')
+
+menuIcon.addEventListener('click', toggleMenu)
+closeBtn.addEventListener('click', toggleMenu)
+
+
+function toggleMenu() {
+	if (menu == 'closed') {
+		navigation.style.display = 'block'
+		closeBtn.style.display = 'block'
+		menu = 'open'
+	} else {
+		navigation.style.display = 'none'
+		closeBtn.style.display = 'none'
+		menu = 'closed'
+	}
 }
+
+
+// Attaching the event listener function to window's resize event
+
+window.addEventListener("resize", function () {
+let width = document.documentElement.clientWidth;
+	if (width > 1080) {
+		navigation.style.display = 'block'
+	} else {
+		navigation.style.display = 'none'
+	}
+	menu = 'closed'
+	closeBtn.style.display = 'none'
+});
+
+				
